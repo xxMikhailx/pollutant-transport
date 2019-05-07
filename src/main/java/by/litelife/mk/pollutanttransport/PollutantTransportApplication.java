88c9ffcd223889638eb850ae06598a21e5f39506
xxMikhailx <misha.kazuchits@gmail.com>
2019-05-07 11:57:49 +0300

    Initial commit

---
+++ src/main/java/by/litelife/mk/pollutanttransport/PollutantTransportApplication.java
@@ -0,0 +1,13 @@
+package by.litelife.mk.pollutanttransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollutantTransportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollutantTransportApplication.class, args);
+    }
+
+}
