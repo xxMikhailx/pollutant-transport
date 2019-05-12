3182426fd284944802bc0fea7d21e76b099578d6
Ivan Yarochkin <Ivan.Yarochkin97@gmail.com>
2019-05-12 15:06:19 +0300

    Add an ability to draw geojson from the server

--- src/main/java/by/litelife/mk/pollutanttransport/MainController.java
+++ src/main/java/by/litelife/mk/pollutanttransport/MainController.java
@@ -1,8 +1,12 @@
 package by.litelife.mk.pollutanttransport;
 
+import by.litelife.mk.pollutanttransport.model.InputData;
+import by.litelife.mk.pollutanttransport.util.GenerateSimpleGeojson;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,6 +14,7 @@ public class MainController {
 
     @GetMapping(value = "/")
     public ModelAndView main(Model model) {
+        model.addAttribute("data", new InputData());
         return new ModelAndView("index");
     }
 
@@ -18,4 +23,10 @@ public class MainController {
         return new ModelAndView("static/about-application");
     }
 
+    @PostMapping(value = "/simulate")
+    public String simulate(Model model, @ModelAttribute("data") InputData inputData) {
+        model.addAttribute("simulatedGeojson", GenerateSimpleGeojson.generateGeojson());
+        model.addAttribute("inputData", inputData);
+        return "index";
+    }
 }
---
+++ src/main/java/by/litelife/mk/pollutanttransport/model/InputData.java
@@ -0,0 +1,49 @@
+package by.litelife.mk.pollutanttransport.model;
+
+public class InputData {
+    private double windSpeed;
+    private double waterFlow;
+    private double riverSpeed;
+    private double radius;
+    private double time;
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(double windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+
+    public double getWaterFlow() {
+        return waterFlow;
+    }
+
+    public void setWaterFlow(double waterFlow) {
+        this.waterFlow = waterFlow;
+    }
+
+    public double getRiverSpeed() {
+        return riverSpeed;
+    }
+
+    public void setRiverSpeed(double riverSpeed) {
+        this.riverSpeed = riverSpeed;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+}
