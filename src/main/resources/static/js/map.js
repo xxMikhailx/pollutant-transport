c102b2e9d1983ede3943ca6867b5cfcd382b08e2
Ivan Yarochkin <Ivan.Yarochkin97@gmail.com>
2019-05-12 17:34:55 +0300

    Add Svisloch river to the map

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -1,3 +1,4 @@
 var mymap = L.map('mainMap').setView([53.902334, 27.5618791], 7);
 
 L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mymap);
+L.geoJson(svislochRiver).addTo(mymap);
