b874a9f58867a853c38fd1eef0e8c4d5f91a0a67
Ivan Yarochkin <Ivan.Yarochkin97@gmail.com>
2019-05-16 04:34:48 +0300

    Add concentration decrease over time validation

--- src/main/resources/static/js/validation.js
+++ src/main/resources/static/js/validation.js
@@ -12,6 +12,7 @@ function validateData() {
     validateRiverSpeedData($(".river-speed"));
     showOrHideErrorMessage($(".incorrect-input-values-error-message"), isCorrect);
     validateTimeRepeatable();
+    validateConcentrationDecreaseOverTime();
 }
 
 function validateConcentrationData(element) {
@@ -32,6 +33,28 @@ function validateTimeRepeatable() {
     var errorMessage = $(".unique-time-value-error-message");
     var timeValues = $(".time-input").map((idx, elem) => $(elem).val()).get();
     showOrHideErrorMessage(errorMessage, isUniqueArray(timeValues));
+}
+
+function validateConcentrationDecreaseOverTime () {
+    var timeConcentrationPairs = new Map();
+    var timeList = $(".time-input").map((idx, elem) => $(elem).val()).get();
+    var concentrationList = $(".concentration-input").map((idx, elem) => $(elem).val()).get();
+    var isConcentrationDecreaseOverTime = true;
+
+    for (var i = 0; i < timeList.length; i++) {
+        timeConcentrationPairs.set(timeList[i], concentrationList[i]);
+    }
+
+    var mapValues = Array.from(new Map([...timeConcentrationPairs.entries()].sort()).values());
+
+    for (var i = 0; i < mapValues.length - 1; i++) {
+        if(parseFloat(mapValues[i]) < parseFloat(mapValues[i+1])) {
+            isConcentrationDecreaseOverTime = false
+            isCorrect = false;
+        }
+    }
+
+    showOrHideErrorMessage($(".concentration-decrease-over-time-error-message"), isConcentrationDecreaseOverTime);
 }
 
 function markInputByValidationResult(input, isValid) {
