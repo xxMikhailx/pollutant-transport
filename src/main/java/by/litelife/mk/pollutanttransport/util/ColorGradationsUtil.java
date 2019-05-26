ad1f0472fdc4582c285e3dadccc391161175ea01
Ivan Yarochkin <Ivan.Yarochkin97@gmail.com>
2019-05-26 20:22:33 +0300

    Add reset data button

--- src/main/java/by/litelife/mk/pollutanttransport/util/ColorGradationsUtil.java
+++ src/main/java/by/litelife/mk/pollutanttransport/util/ColorGradationsUtil.java
@@ -9,10 +9,10 @@ public final class ColorGradationsUtil {
     private static final Map<DoubleRange, String> concentrationColorMap = new HashMap<>();
 
     static {
-        concentrationColorMap.put(new DoubleRange(0.00001, 0.0000001), "#00C711");
+        concentrationColorMap.put(new DoubleRange(0.00001, 0.00004), "#00C711");
         concentrationColorMap.put(new DoubleRange(0.00004, 0.0001), "#22AE12");
         concentrationColorMap.put(new DoubleRange(0.0001, 0.0003), "#459514");
-        concentrationColorMap.put(new DoubleRange(0.0003, 0.0008), "#677C16");
+        concentrationColorMap.put(new DoubleRange(0.0003, 0.0005), "#677C16");
         concentrationColorMap.put(new DoubleRange(0.0005, 0.001), "#8A6317");
         concentrationColorMap.put(new DoubleRange(0.001, 0.005), "#AC4A19");
         concentrationColorMap.put(new DoubleRange(0.005, 0.1), "#CF311B");
