0aa5dea83910836bc3bf25e9e5d497a684427b2c
xxMikhailx <misha.kazuchits@gmail.com>
2019-06-06 19:27:15 +0300

    Add dto for story chart pair

---
+++ src/main/java/by/litelife/mk/pollutanttransport/model/XYDto.java
@@ -0,0 +1,11 @@
+package by.litelife.mk.pollutanttransport.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class XYDto {
+    private double x;
+    private double y;
+}
